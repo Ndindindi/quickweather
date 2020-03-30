375d791e770fc697a0b7eb9f24e740e5647be0a7
Ndindindi <mbumynaj@gmail.com>
2020-03-30 21:30:28 +0300

    style: added styles

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -1,14 +1,15 @@
 'use-strict'
-import { Dimensions } from 'react-native';
+import {Dimensions} from 'react-native';
 
 
-const { width, height } = Dimensions.get('window');
+const {width, height} = Dimensions.get('window');
 
 export const styles = {
-     slide: {
+    slide: {
         flex: 1,
         backgroundColor: '#783B62',
-         borderRadius: 15
+        borderRadius: 15,
+        paddingHorizontal: 10,
     },
     container: {
         flex: 1,
@@ -33,19 +34,59 @@ export const styles = {
         marginLeft: 7,
         marginRight: 7
     },
-    activeDot:{
+    activeDot: {
         backgroundColor: '#FFF',
         width: 13, height: 13,
         borderRadius: 7,
         marginLeft: 7,
         marginRight: 7
     },
-    outerContainer:{
+    outerContainer: {
         paddingHorizontal: 20,
         paddingTop: 40,
         paddingBottom: 30,
-        backgroundColor:'#4E3A52',
-        height:height*1.05,
+        backgroundColor: '#4E3A52',
+        height: height * 1.05,
         width,
     },
+    card: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    amount: {
+        fontSize: 28,
+        color: '#FFF',
+    },
+    unit: {
+        color: '#FFF',
+        justifyContent: 'flex-end'
+    },
+    titleText:{
+        color: '#FFF',
+        marginLeft: 28,
+        marginTop:5
+    },
+    titleView:{
+        position: 'relative',
+        top: 35,
+        alignSelf: 'center',
+        width:'100%',
+        height: '18%',
+        borderBottomLeftRadius: 5,
+        borderBottomRightRadius: 5
+    },
+    text:{
+        color: '#FFF'
+    },
+    cloudText:{
+        color: '#FFF',
+        fontSize: 30,
+    },
+    tempText:{
+        color: '#FFF',
+        fontSize: 50,
+
+    },
+
 };
