e66f15b1e2be0dcca908a1d5a58bbcd44ea23437
Ndindindi <mbumynaj@gmail.com>
2020-03-30 16:53:12 +0300

    style: added custom styles

---
+++ src/styles/styles.js
@@ -0,0 +1,28 @@
+'use-strict'
+export const styles = {
+    mainScreen : {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    slide: {
+        flex: 1,
+        backgroundColor: 'transparent'
+    },
+    container: {
+        flex: 1,
+    },
+
+    imgBackground: {
+        width,
+        height,
+        backgroundColor: 'transparent',
+        position: 'absolute'
+    },
+
+    image: {
+        width,
+        height,
+    }
+}
