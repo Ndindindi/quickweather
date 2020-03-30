99e5be6146dc60dc47ab4b248ecfed150cb08b4a
Ndindindi <mbumynaj@gmail.com>
2020-03-30 18:07:23 +0300

    style: added styles

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -1,14 +1,14 @@
 'use-strict'
+import { Dimensions } from 'react-native';
+
+
+const { width, height } = Dimensions.get('window');
+
 export const styles = {
-    mainScreen : {
+     slide: {
         flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    slide: {
-        flex: 1,
-        backgroundColor: 'transparent'
+        backgroundColor: '#783B62',
+         borderRadius: 15
     },
     container: {
         flex: 1,
@@ -24,5 +24,28 @@ export const styles = {
     image: {
         width,
         height,
-    }
-}
+    },
+    dotStyle: {
+        backgroundColor: 'rgba(255,255,255,.3)',
+        width: 13,
+        height: 13,
+        borderRadius: 7,
+        marginLeft: 7,
+        marginRight: 7
+    },
+    activeDot:{
+        backgroundColor: '#FFF',
+        width: 13, height: 13,
+        borderRadius: 7,
+        marginLeft: 7,
+        marginRight: 7
+    },
+    outerContainer:{
+        paddingHorizontal: 20,
+        paddingTop: 40,
+        paddingBottom: 30,
+        backgroundColor:'#4E3A52',
+        height:height*1.05,
+        width,
+    },
+};
